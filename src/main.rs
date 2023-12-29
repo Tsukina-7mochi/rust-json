71b5b9d98ea707d7ecce6d9bbdda18aa386d3058
Tsukina-7mochi <tsukina7mochi@gmail.com>
2023-12-29 18:18:19 +0900

    feat: implement test script

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+mod json;
+
+use json::parser::Parser;
+
 fn main() {
-    println!("Hello, world!");
+    let json_data = "
+[
+    { \"name\": \"Alice\", \"age\": 15 },
+    { \"name\": \"Bob\", \"age\": 20 },
+    { \"name\": \"Charlie\", \"age\": 25 }
+]
+    ";
+
+    let obj = Parser::parse(json_data).unwrap();
+    for i in 0..3 {
+        let map = obj.get_as_array(i).unwrap();
+        let name = map.get_as_object("name").unwrap().as_string().unwrap();
+        let age = map.get_as_object("age").unwrap().as_i64().unwrap();
+
+        println!("{} name: {}, age: {}", i, name, age);
+    }
 }
